9d31581d6c93c93a29c3054e7252b2043de42031
Jingjing Yin <hyesung_love1127@163.com>
2018-12-07 22:57:21 +0800

    commit demo 3

--- summarydiff.py
+++ summarydiff.py
@@ -13,26 +13,96 @@ def readfile(filename):
     return content
 
 
-def extract_re(file):
+def extract_re(infile, outfile):
    docid_pat = re.compile(r'.+CWebSummary::.+DocID\((.+)\).+')
    url_pat = re.compile(r'.*\[Url\]: (.+)')
    title_pat = re.compile(r'.*\[Title\]: (.+)')
    summary_pat = re.compile(r'.*\[Summary\]: (.+)')
    tuwen_pat = re.compile(r'.*\[tuwen-Summary\]: (.+)')
 
-   data = readfile(file)
+   f = open(outfile, 'w')
+   data = readfile(infile)
    for line in data:
        if docid_pat.match(line):
-          print "DOCID:" +   docid_pat.match(line).group(1)
+          f.write('\n===\n')
+          f.write('DOCID:' + docid_pat.match(line).group(1))
        elif url_pat.match(line):
-          print "URL:" +   url_pat.match(line).group(1)
+          f.write('URL:' + url_pat.match(line).group(1))
        elif title_pat.match(line):
-          print "TITLE:" +   title_pat.match(line).group(1)
+          f.write('TITLE:' + title_pat.match(line).group(1))
        elif summary_pat.match(line):
-          print "SUMMARY:" +   summary_pat.match(line).group(1)
+          f.write('SUMMARY:' + summary_pat.match(line).group(1))
        elif tuwen_pat.match(line):
-          print "TUWEN:" +   tuwen_pat.match(line).group(1)
-             
+          f.write('TUWEN:' + tuwen_pat.match(line).group(1))
+       else:
+          pass
+   f.close()
+
+def getdiff(testfile, onlinefile):
+   test_res =  readfile(testfile)
+   online_res = readfile(onlinefile)
+   return test_res
+
+def read_file_to_list(file):
+    pat_dict = {'DOCID': r' CWebSummary::.+DocID\(([^)]*)',
+                'URL':   r'\[Url\]: (.*)',
+                'TITLE': r'\[Title\]: (.*)',
+                'SUMMARY': r'\[Summary\]: (.*)',
+                'TUWEN':  r'\[tuwen-Summary\]: (.*)'}
+
+    lists = []
+    node = {}
+    with open(file, 'r') as f:
+        for line in f.readlines():
+            # line --> node
+            #print("-->%s\n" % line)
+            for key in pat_dict.keys():
+                p = re.search(pat_dict[key], line)
+                if p:
+                    print("%s:%s" % (key, p.group(1)))
+                    # 
+                    if key == 'DOCID':
+                        if node is not {}:
+                            lists.append(node)
+                            node = {}
+
+                    node[key] = p.group(1)
+                    break
+
+    print(lists)
+    return lists
+
+def cmp_lists(list1, list2):
+    for i in len(list1):
+        same = True
+        for key in ['DOCID', 'URL', 'TITLE', 'SUMMARY', 'TUWEN']:
+            if list1[i][key] != list2[i][key]:
+                same = False
+                break
+
+        # 
+        if not same:
+            # Translate and format
+            translate_and_format(list1[i])
+            translate_and_format(list2[i])
+
+
+    return
+
+def translate_and_format(node):
+    pass
+    return
 
 if __name__=='__main__':
-    extract_re(sys.argv[1]) 
+   #test 
+   #extract_re(sys.argv[1], sys.argv[2])
+   #online
+   #extract_re(sys.argv[1], sys.argv[3])
+   #res = getdiff(sys.argv[1], sys.argv[2])
+   #res = readfile(sys.argv[1])
+   #print res[4]
+   read_file_to_list(sys.argv[1])
+
+
+
+
